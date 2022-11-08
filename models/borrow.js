57e4219682db8e535ce32ae7026d9a3e2216cae9
kkafi09 <afi.firda@gmail.com>
2022-11-08 07:58:21 +0700

    add: setup a database

---
+++ models/borrow.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class borrow extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  borrow.init({
+    memberID: DataTypes.INTEGER,
+    adminID: DataTypes.INTEGER,
+    date_of_borrow: DataTypes.DATE,
+    date_of_return: DataTypes.DATE,
+    status: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'borrow',
+  });
+  return borrow;
+};
---
+++ models/details_of_borrow.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class details_of_borrow extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  details_of_borrow.init({
+    borrowID: DataTypes.INTEGER,
+    bookID: DataTypes.INTEGER,
+    qty: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'details_of_borrow',
+  });
+  return details_of_borrow;
+};
